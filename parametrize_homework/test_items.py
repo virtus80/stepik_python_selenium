51bffdd44f9869b31c60eb44cf6da570f43a92e5
virtus80 <virtus80@mail.ru>
2019-07-21 21:02:02 +0300

    added option browser_name to parametrize_homework; fixed link

--- parametrize_homework/test_items.py
+++ parametrize_homework/test_items.py
@@ -1,9 +1,8 @@
 import time
 
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_display_for_different_languages(browser):
     browser.get(link)
-    time.sleep(10)
     add_button = browser.find_element_by_css_selector("button.btn-add-to-basket")
     assert add_button is not None
