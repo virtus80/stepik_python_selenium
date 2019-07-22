537218fc44aaee6ee22f0749dd65d974cc36bec0
Oleksandr Pletnov <virtus80@mail.ru>
2019-07-22 12:28:49 +0300

    added assertions for elements on login page

--- page_object/pages/locators.py
+++ page_object/pages/locators.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators(object):
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTRATION_FORM = (By.ID, "register_form")
---
+++ page_object/pages/login_page.py
@@ -0,0 +1,18 @@
+from page_object.pages.locators import LoginPageLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert self.browser.current_url.contains("login"), "Page URL doesn\'t contain login"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not displayed"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not displayed"
