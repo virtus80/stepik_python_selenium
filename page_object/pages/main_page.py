023ad47647aebe0765fdd3e3a724ac5112978f26
Oleksandr Pletnov <virtus80@mail.ru>
2019-07-22 12:50:48 +0300

    added moving beetween pages in tests

--- page_object/pages/main_page.py
+++ page_object/pages/main_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .login_page import LoginPage
 from .locators import MainPageLocators
 
 class MainPage(BasePage):
@@ -6,6 +7,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
